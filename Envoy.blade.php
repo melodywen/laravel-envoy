348d791aaeda8c0c1d14920e209b2e1a98c39878
mr.wen <604989282@qq.com>
2018-07-11 19:01:58 +0800

    完善 web初始化

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -9,16 +9,24 @@ $php_version=7.1        // php版本
     sudo su
     apt-get update
 
-     {{--1. 安装 mysql 时候需要输入密码--}}
+    {{--1. 移除 apache2--}}
+    apt-get purge apache2 -y
+
+    {{-- 2. Force Locale--}}
+    apt-get install -y language-pack-en-base
+    locale-gen en_US.UTF-8
+
+    {{--3. Set My Timezone--}}
+    ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+
+     {{--4. 安装 mysql 时候需要输入密码--}}
     sudo debconf-set-selections <<< 'mysql-server-5.6 mysql-server/root_password password 123456'
     sudo debconf-set-selections <<< 'mysql-server-5.6 mysql-server/root_password_again password 123456'
 
-    {{--2. 安装常见的web命令--}}
+    {{--5. 安装web 应用以及基础应用--}}
     apt-get -y install nginx mysql-server mysql-client  redis-server git curl unzip vim
 
-    {{--3. 安装指定版本的php--}}
-    apt-get install -y language-pack-en-base
-    locale-gen en_US.UTF-8
+    {{--6. 安装指定版本的php--}}
     apt-get install -y software-properties-common
     LC_ALL=en_US.UTF-8 add-apt-repository ppa:ondrej/php
     apt-get update
@@ -27,7 +35,18 @@ $php_version=7.1        // php版本
 
     apt-get install -y mcrypt php{{$php_version}}-mcrypt php{{$php_version}}-mysql \
     php{{$php_version}}-curl php{{$php_version}}-gd \
-    php{{$php_version}}-dom php{{$php_version}}-mbstring
+    php{{$php_version}}-dom php{{$php_version}}-mbstring \
+    php{{$php_version}}-pgsql php{{$php_version}}-sqlite3 \
+    php{{$php_version}}-apcu php{{$php_version}}-mcrypt \
+    php{{$php_version}}-imap php{{$php_version}}-memcached \
+    php{{$php_version}}-readline php{{$php_version}}-xdebug \
+    php{{$php_version}}-xml php{{$php_version}}-zip \
+    php{{$php_version}}-intl php{{$php_version}}-bcmath \
+    php{{$php_version}}-soap
+
+    {{--7. 安装compser--}}
+    curl -sS https://getcomposer.org/installer | php
+    mv composer.phar /usr/local/bin/composer
 
     echo '-----------------------------------------------------------------'
     echo '| environment install success!!! congratulation you ^_^ ^_^ ^_^ | '
--- readme.md
+++ readme.md
@@ -1,3 +1,12 @@
 # laravel 中 envoy 远程部署的示例代码；
 
-##
+## 第一部分 对 envoy.blade.php 的概述
+### 1. 全局变量的定义：
+```$xslt
+$php_version=7.1        // php版本
+```
+### 2. 命令的介绍
+1.web环境的初始化: 会安装 nginx mysql redis php  git composer 等等
+```$xslt
+ envoy run env-init 
+```
