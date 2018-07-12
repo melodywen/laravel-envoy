07f1d243d321fa9e6eb07c8c95674d74f59e7bf2
mr.wen <604989282@qq.com>
2018-07-12 16:23:36 +0800

    完成站点配置

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,6 +1,19 @@
-{{
-$php_version=7.1        // php版本
-}}
+@setup
+    // 默认 初始化web环境的php版本
+    $php_version=7.1;
+
+    // ngnix 的站点配置 可以传递的参数有  $host 、$phpFPM 、$projectName
+    $rootDir = '/vagrant';
+
+    $host = $host ?? 'localhost';
+    $phpFPM = $phpFPM ?? $php_version;
+    $phpFPM = trim($phpFPM,'php');
+    $projectName = $projectName ?? 'laravel';
+
+    $realPath = rtrim($rootDir,'/').'/'. $projectName . '/public';
+    $realPathPattern = str_replace('/','\/',$realPath);
+
+@endsetup
 
 @servers(['web' => 'root@192.168.33.10'])
 
@@ -62,4 +75,55 @@ $php_version=7.1        // php版本
 {{--composer 切换官方镜像--}}
 @task('composer-office-mirror',['on' => 'web'])
     composer config -g repo.packagist composer https://packagist.org
+@endtask
+
+@task('nginx-site-add',['on' => 'web'])
+    cd /etc/nginx/sites-available
+    {{--1. 强行新建一个站点文件--}}
+    cp default {{ $host }}
+    {{--2. 修改文件中的内容--}}
+    sed -i "/^#/d" {{ $host }}
+    sed -i "s/listen 80 default_server/listen 80/" {{ $host }}
+    sed -i "/listen \[::\]:80 default_server/"d {{ $host }}
+    sed -i "s/root \/var.*/root {{ $realPathPattern }};/" {{ $host }}
+    sed -i "s/index index.html/index index.php index.html/" {{ $host }}
+    sed -i "s/server_name _/server_name {{ $host }}/" {{ $host }}
+
+    sed -i "/try_files \$uri \$uri\/ =404/a\ \t\t try_files \$uri \$uri\/ \/index.php?\$query_string;" {{ $host }}
+    sed -i "s/try_files \$uri \$uri\/ =404/#try_files \$uri \$uri\/ =404/" {{ $host }}
+
+    sed -i "/location ~ \\\.php/i\ \n\tcharset utf-8; \n" {{ $host }}
+    sed -i "/location ~ \\\.php/i\ \tlocation = \/favicon.ico \{ access_log off; log_not_found off; \}" {{ $host }}
+    sed -i "/location ~ \\\.php/i\ \tlocation = \/robots.txt  \{ access_log off; log_not_found off; \}  \n" {{ $host }}
+    sed -i "/location ~ \\\.php/i\ \terror_page 404 \/index.php;  \n" {{ $host }}
+    sed -i "/location ~ \\\.php/i\ \taccess_log /var/log/nginx/{{ $host }}-access.log;  \n" {{ $host }}
+    sed -i "/location ~ \\\.php/i\ \terror_log /var/log/nginx/{{ $host }}-error.log error;  \n" {{ $host }}
+
+
+    sed -i "/location ~ \\\.php/a\ \t\tfastcgi_split_path_info ^(.+\.php)(/.+)$;" {{ $host }}
+    sed -i "s/#location ~ \\\.php/location ~ \\\.php/" {{ $host }}
+    sed -i "s/#\(.*include snippets\/fastcgi-php\.conf\)/\1/" {{ $host }}
+    sed -i "s/#\(.*fastcgi_pass unix:\/run\/php\/php\)7\.0\(-fpm\.sock\)/\1{{ $phpFPM }}\2/" {{ $host }}
+
+
+    sed -i "s/#location ~ \/\\\.ht/location ~ \/\.(?!well-known).*/" {{ $host }}
+    sed -i "s/#\(.*deny all;\)/\1/" {{ $host }}
+
+    sed -i "s/#}/}/g" {{ $host }}
+
+    cd /etc/nginx/sites-enabled
+    rm -f {{$host}}
+    ln -s /etc/nginx/sites-available/{{$host}}
+
+    nginx -t
+    nginx -s stop
+    nginx
+
+    echo '-----------------------------------------------------------------'
+    echo '| nginx site create success !!! congratulation you,  ^_^ ^_^ ^_^  '
+    echo '| host: {{ $host }}                                               '
+    echo '| php-fpm: php-{{ $phpFPM }}                                      '
+    echo '| projectName: {{ $projectName }}                                 '
+    echo '| realPath: {{ $realPath }}                                       '
+    echo '-----------------------------------------------------------------'
 @endtask
--- readme.md
+++ readme.md
@@ -15,3 +15,10 @@ $php_version=7.1        // php版本
     envoy run composer-china-mirror   切换中国镜像
     envoy run composer-office-mirror  切换官方镜像
     ```
+3. 新建nginx站点 (新建一个站点需要传递 fpm值 [php5.6,php7.0,php7.1,php7.2]，项目名称、域名)
+    ```$xslt    
+    envoy run nginx-site-add  --phpFPM=php7.0 --projectName=laravel --host=www.cjw.com
+    ```
+    * 此站点配置主要是针对与laravel 的项目站点配置，它会自动配置到项目的public下面
+    * 根路径已经是设置好了，默认值是 `/vagrant`,如果是线上，请改为 `/var/www`
+    * 同一个主机里，不同的项目可以使用 不同的php版本。指定phpfpm版本即可
