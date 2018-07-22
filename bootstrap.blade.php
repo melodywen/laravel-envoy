43101894fc1de59ce444827253cd3217982ff2aa
mr.wen <604989282@qq.com>
2018-07-22 13:32:10 +0800

    文件切割

---
+++ bootstrap.blade.php
@@ -0,0 +1,8 @@
+
+@import('vendor/mr-jiawen/laravel-envoy/src/default_setup.blade.php')
+
+@import('vendor/mr-jiawen/laravel-envoy/src/linux_tasks.blade.php')
+
+@import('vendor/mr-jiawen/laravel-envoy/src/nginx_tasks.blade.php')
+
+@import('vendor/mr-jiawen/laravel-envoy/src/php_tasks.blade.php')
