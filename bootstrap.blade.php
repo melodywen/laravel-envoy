afb754c0bdadee5ccfb117cd7e44ec7fdde878d3
mr.wen <604989282@qq.com>
2018-07-22 15:13:08 +0800

    更换apt 源

--- bootstrap.blade.php
+++ bootstrap.blade.php
@@ -1,8 +1,12 @@
+@setup
+    $envoy_system_url = 'vendor/mr-jiawen/laravel-envoy/';
+    $envoy_system_url = rtrim($envoy_system_url, '/');
+@endsetup
 
-@import('vendor/mr-jiawen/laravel-envoy/src/default_setup.blade.php')
+@import( $envoy_system_url . '/src/default_setup.blade.php')
 
-@import('vendor/mr-jiawen/laravel-envoy/src/linux_tasks.blade.php')
+@import( $envoy_system_url . '/src/linux_tasks.blade.php')
 
-@import('vendor/mr-jiawen/laravel-envoy/src/nginx_tasks.blade.php')
+@import( $envoy_system_url .'/src/nginx_tasks.blade.php')
 
-@import('vendor/mr-jiawen/laravel-envoy/src/php_tasks.blade.php')
+@import( $envoy_system_url .'/src/php_tasks.blade.php')
--- src/linux_tasks.blade.php
+++ src/linux_tasks.blade.php
@@ -1,8 +1,5 @@
 {{--1. linux 的基础操作 --}}
 @task('task-linux-general')
-
-    apt-get update
-
     {{--1. 移除 apache2--}}
     apt-get purge apache2 -y
 
@@ -13,4 +10,42 @@
     {{--3. Set My Timezone--}}
     ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 
+@endtask
+
+{{--2. ubuntu 换源 阿里源--}}
+@task('task-linux-apt-sources-aliyun')
+    {{--1. 移除锁文件--}}
+    rm -rf /var/lib/apt/lists/lock
+    {{--2. 换源--}}
+    echo 'deb http://mirrors.aliyun.com/ubuntu/ xenial main restricted universe multiverse' > /etc/apt/sources.list
+    echo 'deb http://mirrors.aliyun.com/ubuntu/ xenial-security main restricted universe multiverse' >> /etc/apt/sources.list
+    echo 'deb http://mirrors.aliyun.com/ubuntu/ xenial-updates main restricted universe multiverse' >> /etc/apt/sources.list
+    echo 'deb http://mirrors.aliyun.com/ubuntu/ xenial-proposed main restricted universe multiverse' >> /etc/apt/sources.list
+    echo 'deb http://mirrors.aliyun.com/ubuntu/ xenial-backports main restricted universe multiverse' >> /etc/apt/sources.list
+    echo 'deb-src http://mirrors.aliyun.com/ubuntu/ xenial main restricted universe multiverse' >> /etc/apt/sources.list
+    echo 'deb-src http://mirrors.aliyun.com/ubuntu/ xenial-security main restricted universe multiverse' >> /etc/apt/sources.list
+    echo 'deb-src http://mirrors.aliyun.com/ubuntu/ xenial-updates main restricted universe multiverse' >> /etc/apt/sources.list
+    echo 'deb-src http://mirrors.aliyun.com/ubuntu/ xenial-proposed main restricted universe multiverse' >> /etc/apt/sources.list
+    echo 'deb-src http://mirrors.aliyun.com/ubuntu/ xenial-backports main restricted universe multiverse' >> /etc/apt/sources.list
+    {{--3. 更新--}}
+    apt-get update
+@endtask
+
+{{--2. ubuntu 换源 官方源--}}
+@task('task-linux-apt-sources-office')
+    {{--1. 移除锁文件--}}
+    rm -rf /var/lib/apt/lists/lock
+    {{--2. 换源--}}
+    echo 'deb http://us.archive.ubuntu.com/ubuntu/ xenial main restricted' > /etc/apt/sources.list
+    echo 'deb http://us.archive.ubuntu.com/ubuntu/ xenial-updates main restricted' >> /etc/apt/sources.list
+    echo 'deb http://us.archive.ubuntu.com/ubuntu/ xenial universe' >> /etc/apt/sources.list
+    echo 'deb http://us.archive.ubuntu.com/ubuntu/ xenial-updates universe' >> /etc/apt/sources.list
+    echo 'deb http://us.archive.ubuntu.com/ubuntu/ xenial multiverse' >> /etc/apt/sources.list
+    echo 'deb http://us.archive.ubuntu.com/ubuntu/ xenial-updates multiverse' >> /etc/apt/sources.list
+    echo 'deb http://us.archive.ubuntu.com/ubuntu/ xenial-backports main restricted universe multiverse' >> /etc/apt/sources.list
+    echo 'deb http://security.ubuntu.com/ubuntu xenial-security main restricted' >> /etc/apt/sources.list
+    echo 'deb http://security.ubuntu.com/ubuntu xenial-security universe' >> /etc/apt/sources.list
+    echo 'deb http://security.ubuntu.com/ubuntu xenial-security multiverse' >> /etc/apt/sources.list
+    {{--3. 更新--}}
+    apt-get update
 @endtask
